0be28a6b0c29ee8b8a2a5f6afb227cf22e7a81b4
Rajat Kant <rajatkant@Rajats-MacBook-Air.local>
2024-09-07 23:41:06 +0530

    fix aoi call for rest menu too

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -16,13 +16,13 @@ const RestaurantMenu = () => {
   if (resInfo === null) return <Shimmer />;
 
   const { name, cuisines, costForTwoMessage } =
-    resInfo?.cards[0]?.card?.card?.info;
+    resInfo?.cards[2]?.card?.card?.info;
 
   const { itemCards } =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
   const categories =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (c) =>
         c.card?.["card"]?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
@@ -36,7 +36,7 @@ const RestaurantMenu = () => {
         {cuisines.join(", ")} - {costForTwoMessage}
       </p>
       {/* categories accordions */}
-      {categories.map((category, index) => (
+      {categories?.map((category, index) => (
         // controlled component
         <RestaurantCategory
           key={category?.card?.card.title}
